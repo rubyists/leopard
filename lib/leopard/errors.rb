df0fee8373fc0088cd61abcc3e6d963e957730b6
bougyman <bougyman@users.noreply.github.com>
2025-08-16 13:00:13 -0400

    fix: #35 - Limits the backtrace to 4 lines in our exceptions (#36)
    * fix: #35 - Limits the backtrace to 4 lines in our exceptions
    
    * fix: #35 - Adds test for backtrace truncation
    
    * fix: #35 - Adds nil check in case backtrace is empty
    
    * chore: Bumps leopard version in bundle
    
    * fix: #35 - Do not add a truncate message if we did not truncate it

--- lib/leopard/errors.rb
+++ lib/leopard/errors.rb
@@ -2,7 +2,24 @@
 
 module Rubyists
   module Leopard
-    class Error < StandardError; end
+    class LeopardError < StandardError
+      def initialize(...)
+        super
+        set_backtrace(caller)
+      end
+
+      def backtrace
+        # If the backtrace is nil, return an empty array
+        orig = (super || [])[0..3]
+        # If the backtrace is less than 4 lines, return it as is
+        return orig if orig.size < 4
+
+        # Otherwise, add a note indicating truncation
+        orig + ['... (truncated by Leopard)']
+      end
+    end
+
+    class Error < LeopardError; end
     class ConfigurationError < Error; end
     class ResultError < Error; end
   end
