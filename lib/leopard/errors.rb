c8b88a451332c76b851cc95976b751b4bf93ae40
Tj (bougyman) Vanderpoel <bougyman@users.noreply.github.com>
2025-08-16 10:46:26 -0500

    fix: #35 - Limits the backtrace to 4 lines in our exceptions

--- lib/leopard/errors.rb
+++ lib/leopard/errors.rb
@@ -2,7 +2,13 @@
 
 module Rubyists
   module Leopard
-    class Error < StandardError; end
+    class LeopardError < StandardError
+      def backtrace
+        super[0..3] + ['... (truncated by Leopard)']
+      end
+    end
+
+    class Error < LeopardError; end
     class ConfigurationError < Error; end
     class ResultError < Error; end
   end
