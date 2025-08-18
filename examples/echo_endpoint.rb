a2e239b92473adc1cf9736323f99d74d57d687ed
bougyman <bougyman@users.noreply.github.com>
2025-08-18 10:09:13 -0400

    chore: Update bundle dependencies (#38)
    * chore: Update bundle dependencies
    
    * feat: Adds logging to example service

--- examples/echo_endpoint.rb
+++ examples/echo_endpoint.rb
@@ -7,16 +7,26 @@ require_relative '../lib/leopard/nats_api_server'
 class EchoService
   include Rubyists::Leopard::NatsApiServer
 
+  config.logger = SemanticLogger[:EchoService]
   def initialize(a_var: 1)
     logger.info "EchoService initialized with a_var: #{a_var}"
   end
 
-  endpoint(:echo) { |msg| Success(msg.data) }
-  endpoint(:echo_fail) { |msg| Failure({ failure: '*boom*', data: msg.data }) }
+  endpoint(:echo) do |msg|
+    data = msg.data
+    logger.trace('Received message', data:)
+    Success(data)
+  end
+  endpoint(:echo_fail) do |msg|
+    data = msg.data
+    logger.trace('Received message', data:)
+    Failure({ failure: '*boom*', data: msg.data })
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   SemanticLogger.default_level = :info
+  SemanticLogger.add_signal_handler
   SemanticLogger.add_appender(io: $stdout, formatter: :color)
   EchoService.run(
     nats_url: 'nats://localhost:4222',
@@ -25,6 +35,6 @@ if __FILE__ == $PROGRAM_NAME
       version: '1.0.0',
       instance_args: { a_var: 2 },
     },
-    instances: 1,
+    instances: ENV.fetch('ECHO_INSTANCES', '1').to_i,
   )
 end
