0af4b0ba3c322ad734730f45eac925a73a760c73
Gabriel Odess-Gillett <gabeodess@gmail.com>
2026-09-17 14:44:14 -0500

    fix: content type calculation

--- lib/leopard/metrics_server.rb
+++ lib/leopard/metrics_server.rb
@@ -63,8 +63,8 @@ module Rubyists
       def write_metrics_response(client, request_line, workers)
         if request_line&.start_with?('GET /metrics')
           body = prometheus_metrics(workers)
-          body = JSON.generate(body) unless body.is_a?(String)
           content_type = body.is_a?(String) ? 'text/plain; version=0.0.4' : 'application/json'
+          body = JSON.generate(body) unless body.is_a?(String)
 
           client.write "HTTP/1.1 200 OK\r\n" \
                        "Content-Type: #{content_type}\r\n" \
