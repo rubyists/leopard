bb3b70286fa0d6016d2b5906f97daf0aeccfecb4
Gabriel Odess-Gillett <gabeodess@gmail.com>
2026-09-17 13:07:05 -0500

    chore: rubocop

--- lib/leopard/metrics_server.rb
+++ lib/leopard/metrics_server.rb
@@ -63,7 +63,7 @@ module Rubyists
       def write_metrics_response(client, request_line, workers)
         if request_line&.start_with?('GET /metrics')
           body = prometheus_metrics(workers)
-          body = body.is_a?(String) ? body : JSON.generate(body)
+          body = JSON.generate(body) unless body.is_a?(String)
           client.write "HTTP/1.1 200 OK\r\n" \
                        "Content-Type: text/plain; version=0.0.4\r\n" \
                        "Content-Length: #{body.bytesize}\r\n\r\n#{body}"
