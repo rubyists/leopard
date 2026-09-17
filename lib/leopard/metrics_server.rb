b57c9c7cebd8398e551346b624754245f8459e14
Gabriel Odess-Gillett <gabeodess@gmail.com>
2026-09-17 14:36:12 -0500

    chore: too many lines

--- lib/leopard/metrics_server.rb
+++ lib/leopard/metrics_server.rb
@@ -63,12 +63,8 @@ module Rubyists
       def write_metrics_response(client, request_line, workers)
         if request_line&.start_with?('GET /metrics')
           body = prometheus_metrics(workers)
-          content_type = 'text/plain; version=0.0.4'
-
-          unless body.is_a?(String)
-            body = JSON.generate(body)
-            content_type = 'application/json'
-          end
+          body = JSON.generate(body) unless body.is_a?(String)
+          content_type = body.is_a?(String) ? 'text/plain; version=0.0.4' : 'application/json'
 
           client.write "HTTP/1.1 200 OK\r\n" \
                        "Content-Type: #{content_type}\r\n" \
